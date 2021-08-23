020d4466855bf15b2f5aa3d1b0c4aed57694db27
Crowdbotics <team@crowdbotics.com>
2021-08-23 10:32:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "square_king_30041Identifier",
+  url: "https://square_king_30041Identifier.botics.co",
+  api: "https://square_king_30041Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
